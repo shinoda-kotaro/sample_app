fbcae2de9430c104542c8d973a8b17a1d5327d7b
shinoda-kotaro <shinoda.kotaro.infratop@gmail.com>
2020-03-23 20:56:02 +0900

    [finish]3-6

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   get 'todolists/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+#top画面
   get 'top' => 'homes#top'
-
+#todolistsへの新規投稿
   post 'todolists' => 'todolists#create'
-
+#todolistsの投稿一覧画面
   get 'todolists' => 'todolists#index'
+#todolistsの投稿詳細画面
+  get 'todolists/:id' => 'todolists#show', as:'todolist'
 end
