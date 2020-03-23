547a8476d96050c4775e7ed42628fa955ed2ef4f
shinoda-kotaro <shinoda.kotaro.infratop@gmail.com>
2020-03-23 22:13:09 +0900

    [finish]3-8

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'b5be2f3030056415d72ce60bef7840579ba0a75536803e7609ce74e3514696af0ade1b197dffbc5a2ed3ccb01296177165bcce26e727baebf7fbd9a9fa153839'
